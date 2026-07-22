a720003b7cfbe9c5f1d083c9bebe908b1c37ea53
sambhav sharma <174787750+cipherpunk-dev@users.noreply.github.com>
2026-07-22 14:43:45 +0530

    Changes in config.py: Adjusted for .env

--- shortener_app/config.py
+++ shortener_app/config.py
@@ -1,10 +1,17 @@
 from pydantic import BaseSettings
-class Settings(BaseSettings):
+from functools import lru_cache
+from pathlib import Path
+
+ENV_PATH = Path(__file__).resolve().parent.parent/".env" #Relative path to .env added
+class Settings(BaseSettings):     #basic pydantic validation
     env_name:str ="Local"
     base_url:str ="http://localhost:8000"
     db_url:str ="sqlite:///./shortener.db"
-
-def get_settings()-> Settings:
+    class Config:        #if .env exist take values from there
+        env_file=str(ENV_PATH)
+        case_sensitive=False     #freedom to name .env any case(keeping the name same though)
+@lru_cache
+def get_settings()-> Settings:   #just a routine to print local settings
     settings=Settings()
     print(f"Loading settings for: {settings.env_name}")
     return settings
